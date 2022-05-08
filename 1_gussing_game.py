d69f6207f7e4ba0754e348d48ce6c0bf4fdfbb2c
angus.ku <kjm15246@gmail.com>
2022-05-08 23:13:23 +0800

    Add gussing_game exercise

---
+++ 1_gussing_game.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+import random
+
+
+def guessing_game():
+    ans = random.randint(0, 100)
+    print ("guessing game ans: %d" %(ans))
+    while True:
+        num = input("input val: ")
+        num = int(float(num))
+
+        if (num == ans):
+            print ("You got it, Congratulation")
+            break
+        elif (num > ans):
+            print ("Your value is bigger than ANS")
+        else:
+            print ("Your value is smaller than ANS")
+
+
+guessing_game()
